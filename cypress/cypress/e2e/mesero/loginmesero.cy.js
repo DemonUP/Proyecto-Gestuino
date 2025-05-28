ad272ab393e5bd1bcd60e2a7e5e0a30870bf1ea4
DemonUP <nontrake1@gmail.com>
2025-05-28 18:51:38 -0500

    mejora_carpeta

---
+++ cypress/cypress/e2e/mesero/loginmesero.cy.js
@@ -0,0 +1,15 @@
+describe('Login en Gestuino', () => {
+  it('Debe completar el login con usuario válido', () => {
+    // 1. Abre la pantalla de login
+    cy.visit('https://gestuino.netlify.app/'); // Ajusta la URL si es diferente
+
+    // 2. Escribe el usuario
+    cy.get('input[placeholder="Usuario"]').type('mesero');
+
+    // 3. Escribe la contraseña
+    cy.get('input[placeholder="••••••••"]').type('123');
+
+    // 4. Haz clic en el botón "Acceder al sistema"
+    cy.contains('Acceder al sistema').click();
+  });
+});
